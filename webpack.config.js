f192326268269c3ddc0bfcc56c1ac8e07fcda5d6
DPende <pendesini.daniele@gmail.com>
2024-10-02 13:53:06 +0200

    chore: initial project settings

---
+++ webpack.config.js
@@ -0,0 +1,71 @@
+import path from 'path';
+import CopyPlugin from 'copy-webpack-plugin';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import TerserPlugin from 'terser-webpack-plugin';
+
+export default {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve('dist'),
+        filename: 'app.js',
+        clean: true
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+        new CopyPlugin({
+            patterns: [{
+                from: path.resolve('./manifest.json'),
+                to: path.resolve('dist')
+            },
+            {
+                from: path.resolve('./public'),
+                to: path.resolve('dist/public')
+            }]
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader', 'postcss-loader'],
+            },
+            {
+                test: /\.svg$/,
+                use: [
+                    {
+                        loader: 'svg-url-loader',
+                        options: {
+                            limit: 10000,
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new TerserPlugin({
+                extractComments: false,
+                terserOptions: {
+                    format: {
+                        comments: false,
+                    },
+                },
+            }),
+        ],
+    },
+    mode: 'production',
+};
